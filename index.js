2814e34113281872e95759cd1f44f4bb76b77a9a
Natalia Kazakevich <Natallia_Kazakevich@mail.ru>
2018-09-23 01:08:20 +0300

    whooray!

--- index.js
+++
@@ -1,6 +0,0 @@
-function convertTemp(degree) {  
-    if (degree == "C") {   
-         F = document.getElementById("c").value * 9 / 5 + 32; 
-            document.getElementById("f").value = Math.round(F);
-    }
-}
